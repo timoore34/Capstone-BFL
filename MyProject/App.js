8f0a6ffe505f32f71b6809d75225202d83b328bf
TinaMoore <TMoore@student.neumont.edu>
2020-03-31 20:48:30 -0600

    First push
    Working react native - start app

---
+++ MyProject/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, World!</Text>
+      <Text>Does this apply real time?</Text>
+      <Text>No fucking way!</Text>
+      <Text>Just another test</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
